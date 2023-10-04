8b65adbe9fc9afa209db0a6148b71e43cdd8b35f
frvaldiviap <fr.valdiviap@duocuc.cl>
2023-10-04 14:02:18 -0300

    Se crea contraseña segura y se inicia sesión correctamente

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 # Se importa la clase Flask
 import json
-from flask import Flask, redirect, render_template, request, send_from_directory, url_for
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 # Se inicializa la aplicación
@@ -11,6 +12,8 @@ app = Flask(__name__)
 # Configura la conexión a MySQL
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:afterlife1998@localhost/sys'
 db = SQLAlchemy(app)
+#Clave secreta
+app.secret_key = 'DuocUC2023'
 
 @app.before_request
 def before_request():
@@ -37,6 +40,15 @@ def index():
 
 @app.route('/home')
 def home():
+    usuario_id = session.get('usuario_id')
+    if usuario_id:
+        # Recuperar el usuario de la base de datos utilizando el ID
+        usuario = Usuario.query.get(usuario_id)
+
+        if usuario:
+            # Renderizar la plantilla HTML y pasar el objeto 'usuario' a la plantilla
+            return render_template('home.html', usuario=usuario)
+
     return render_template('home.html')
 
 # Se pone la ruta a la vista mediante el decorador @app.route()
@@ -58,6 +70,7 @@ def query_string():
     return "Ok"
 
 # Definición de la clase para el modelo Tipo_Usuario
+# IMPORTANTE: EN LA BASE DE DATOS DEBEN HABER DATOS EN TIPO_USUARIO
 class TipoUsuario(db.Model):
     __tablename__ = 'TIPO_USUARIO'
     tipo_usuario_id = db.Column(db.Integer, primary_key=True)
@@ -72,15 +85,17 @@ class Usuario(db.Model):
     nombre_usuario = db.Column(db.String(45), nullable=False)
     apellido_usuario = db.Column(db.String(45), nullable=False)
     correo_usuario = db.Column(db.String(45), nullable=False)
+    contrasena = db.Column(db.String(200), nullable=False)
     telefono_usuario = db.Column(db.String(10), nullable=False)
     tipo_usuario_id = db.Column(db.Integer, db.ForeignKey('TIPO_USUARIO.tipo_usuario_id'), nullable=False)
     
 
-    def __init__(self, rut_usuario,nombre_usuario,apellido_usuario,correo_usuario,telefono_usuario,tipo_usuario_id=2):
+    def __init__(self, rut_usuario,nombre_usuario,apellido_usuario,correo_usuario,contrasena,telefono_usuario,tipo_usuario_id=2):
         self.rut_usuario = rut_usuario
         self.nombre_usuario = nombre_usuario
         self.apellido_usuario = apellido_usuario
         self.correo_usuario = correo_usuario
+        self.contrasena = contrasena
         self.telefono_usuario = telefono_usuario
         self.tipo_usuario_id = tipo_usuario_id
 
@@ -88,20 +103,24 @@ class Usuario(db.Model):
 # Vista Inicio Sesión
 @app.route('/inicio-sesion', methods=['GET', 'POST'])
 def login():
-    error = None
+    usuario = None
     if request.method == 'POST':
-        email = request.form['email']
+        # Obtener los datos del formulario de inicio de sesión
+        email =  request.form['email']
         contrasena = request.form['password']
+        #Buscar un usuario con el email
+        usuario = Usuario.query.filter_by(correo_usuario=email).first()
 
-        # Validación de credenciales
-        if email == 'usuario@gmail.com' and contrasena == 'prueba123':
-            # Autenticación exitosa, redirige a la página de reserva
+        if usuario and check_password_hash(usuario.contrasena, contrasena):
+            # Iniciar sesión para el usuario
+            session['usuario_id'] = usuario.usuario_id
+            flash('Inicio de sesión exitoso.', 'success')
             return redirect(url_for('home'))
-        else:
-            # Credenciales incorrectas, muestra un mensaje de error
-            error = "Credenciales incorrectas. Por favor, inténtalo nuevamente."
+            
+        print('Email o contraseña incorrectos')
+        
 
-    return render_template('inicio-sesion.html', error=error)
+    return render_template('inicio-sesion.html')
 
 # Vista Registro
 @app.route('/registro', methods=['GET','POST'])
@@ -112,17 +131,20 @@ def registro():
         nombre = request.form['nombre']
         apellido = request.form['apellido']
         telefono = request.form['telefono']
-        email= request.form['email']
-        password = request.form['password']
+        email = request.form['email']
+        contrasena = request.form['password']
+        # Se genera una hash seguro de la contraseña
+        contrasena_hash = generate_password_hash(contrasena)
 
         # Creación de un nuevo usuario
         nuevo_usuario = Usuario(
-            rut_usuario=rut, 
-            nombre_usuario=nombre, 
-            apellido_usuario=apellido, 
-            correo_usuario=email, 
-            telefono_usuario=telefono,
-            tipo_usuario_id=2
+            rut_usuario = rut, 
+            nombre_usuario = nombre, 
+            apellido_usuario = apellido, 
+            correo_usuario = email,
+            contrasena = contrasena_hash,
+            telefono_usuario = telefono,
+            tipo_usuario_id = 2
         )
         
         # Agregar al usuario a la sesión y guardar en la base de datos
@@ -135,18 +157,29 @@ def registro():
 #Vista Habitaciones
 @app.route('/habitaciones')
 def habitaciones():
-    usuario_test = {
-        'nombre': 'Juan',
-        'apellido': 'Perez',
-        'email': 'juanperez@gmail.com',
-        'telefono': '123456789'
-    }
+    usuario_id = session.get('usuario_id')
+    if usuario_id:
+        # Recuperar el usuario de la base de datos utilizando el ID
+        usuario = Usuario.query.get(usuario_id)
+
+        if usuario:
+            # Renderizar la plantilla HTML y pasar el objeto 'usuario' a la plantilla
+            return render_template('habitaciones.html', usuario=usuario)
     
-    return render_template('habitaciones.html', usuario_test=usuario_test)
+    return render_template('habitaciones.html')
 
 # Vista Reserva
 @app.route('/reserva', methods=['GET','POST'])
 def reserva():
+    usuario_id = session.get('usuario_id')
+    if usuario_id:
+        # Recuperar el usuario de la base de datos utilizando el ID
+        usuario = Usuario.query.get(usuario_id)
+
+        if usuario:
+            # Renderizar la plantilla HTML y pasar el objeto 'usuario' a la plantilla
+            return render_template('reserva.html', usuario=usuario)
+        
     data = {
         'standard_matrimonial': {
             'precio_noche': 100000,
@@ -158,30 +191,35 @@ def reserva():
             'max_huespedes': 2
         }
     }
-    usuario_test = {
-        'nombre': 'Juan',
-        'apellido': 'Perez',
-        'email': 'juanperez@gmail.com',
-        'telefono': '123456789'
-    }
     if request.method == 'POST':
         return redirect(url_for('pago_reserva'))
     
-    return render_template('reserva.html', data=data, usuario_test=usuario_test)
+    return render_template('reserva.html', data=data)
 
 @app.route('/mis-reservas')
 def mis_reservas():
-    usuario_test = {
-        'nombre': 'Juan',
-        'apellido': 'Perez',
-        'email': 'juanperez@gmail.com',
-        'telefono': '123456789'
-    }
+    usuario_id = session.get('usuario_id')
+    if usuario_id:
+        # Recuperar el usuario de la base de datos utilizando el ID
+        usuario = Usuario.query.get(usuario_id)
 
-    return render_template('mis-reservas.html', usuario_test=usuario_test)
+        if usuario:
+            # Renderizar la plantilla HTML y pasar el objeto 'usuario' a la plantilla
+            return render_template('mis-reservas.html', usuario=usuario)
+
+    return render_template('mis-reservas.html', usuario=usuario)
 
 @app.route('/pago-reserva', methods=['GET','POST'])
 def pago_reserva():
+    usuario_id = session.get('usuario_id')
+    if usuario_id:
+        # Recuperar el usuario de la base de datos utilizando el ID
+        usuario = Usuario.query.get(usuario_id)
+
+        if usuario:
+            # Renderizar la plantilla HTML y pasar el objeto 'usuario' a la plantilla
+            return render_template('pago-reserva.html', usuario=usuario)
+        
     if request.method == 'POST':
         return redirect(url_for('reserva_exitosa'))
 
@@ -190,15 +228,16 @@ def pago_reserva():
 
 @app.route('/reserva-exitosa')
 def reserva_exitosa():
-    # Datos del usuario logeado
-    usuario_test = {
-        'nombre': 'Juan',
-        'apellido': 'Perez',
-        'email': 'juanperez@gmail.com',
-        'telefono': '123456789'
-    }
+    usuario_id = session.get('usuario_id')
+    if usuario_id:
+        # Recuperar el usuario de la base de datos utilizando el ID
+        usuario = Usuario.query.get(usuario_id)
 
-    return render_template('reserva-exitosa.html',usuario_test=usuario_test)
+        if usuario:
+            # Renderizar la plantilla HTML y pasar el objeto 'usuario' a la plantilla
+            return render_template('reserva-exitosa.html', usuario=usuario)
+        
+    return render_template('reserva-exitosa.html')
 
 
 
